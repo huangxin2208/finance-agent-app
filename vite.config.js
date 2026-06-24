143ae6cd7387356756f986593e89da657bfacef1
huangxin2208 <64023104+huangxin2208@users.noreply.github.com>
2026-06-24 09:44:47 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['sb-54loollidpt9.vercel.run']
+    allowedHosts: 'all'
   }
 })
